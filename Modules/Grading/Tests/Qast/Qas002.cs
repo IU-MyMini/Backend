c0527f549c3046a98552e1e25be714b0d2460687
Fil-126 <grigorii_fil@inbox.ru>
2025-11-20 01:18:02 +0800

    Add tests for Qast002_2 and Qast006_1

---
+++ Modules/Grading/Tests/Qast/Qas002.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+
+namespace GradingModule.Tests.Qast;
+
+public class Qas002
+{
+    private readonly HttpClient _httpClient;
+
+    public Qas002()
+    {
+        const string testBaseUrlEnv = "TEST_BASE_URL";
+        var          baseUrl        = Environment.GetEnvironmentVariable(testBaseUrlEnv);
+        if (baseUrl is null)
+            Assert.Fail($"{testBaseUrlEnv} env variable is not defined");
+
+        _httpClient             = new HttpClient();
+        _httpClient.BaseAddress = new Uri(baseUrl);
+    }
+
+    [Fact]
+    public async Task Qast002_2()
+    {
+        const string envVar    = "QAST002_2_ENDPOINTS";
+        var          endpoints = Environment.GetEnvironmentVariable(envVar)?.Split(",");
+
+        if (endpoints is null)
+            Assert.Fail($"{envVar} env variable is not defined");
+
+        var tasks   = endpoints.Select(e => _httpClient.GetAsync(e));
+        var results = await Task.WhenAll(tasks);
+
+        foreach (var resp in results)
+        {
+            Assert.True(
+                resp.StatusCode is HttpStatusCode.Unauthorized,
+                $"Got unauthorized access to {resp.RequestMessage?.Method} {resp.RequestMessage?.RequestUri}"
+            );
+        }
+    }
+}
