d208f05e31dea675c5f31911379ff8c0f510da20
Fil-126 <43411212+Fil-126@users.noreply.github.com>
2025-11-20 02:37:33 +0800

    testing: Add tests for QAST002-2 and QAST006-1 (#1)
    * Add tests for Qast002_2 and Qast006_1
    
    * Fix time for Qast006_1
    
    * Update ci-cd.yml
    
    * Update CI/CD to exclude specific diagnostics
    
    Exclude IDE0055 diagnostics during dotnet format verification.
    
    * Update CI/CD workflow by modifying dotnet format step
    
    * Rename workflow from '.NET test' to '.NET'
    
    * Updated editorconfig
    
    ---------
    
    Co-authored-by: Markov Andrei <38224264+markovav-official@users.noreply.github.com>

---
+++ Modules/Grading/Tests/Qast/Qas006.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+
+using Xunit.Abstractions;
+
+namespace GradingModule.Tests.Qast;
+
+public class Qas006
+{
+    private readonly HttpClient _httpClient;
+    private readonly ITestOutputHelper _output;
+
+    public Qas006(ITestOutputHelper output)
+    {
+        _output = output;
+
+        const string testBaseUrlEnv = "TEST_BASE_URL";
+        var baseUrl = Environment.GetEnvironmentVariable(testBaseUrlEnv);
+        if (baseUrl is null)
+            Assert.Fail($"{testBaseUrlEnv} env variable is not defined");
+
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri(baseUrl);
+    }
+
+    [Fact]
+    public async Task Qast006_1()
+    {
+        const string envVar = "QAST006_1_ENDPOINT";
+        var endpoint = Environment.GetEnvironmentVariable(envVar);
+
+        if (endpoint is null)
+            Assert.Fail($"{envVar} env variable is not defined");
+
+        var testDuration = new TimeSpan(0, 1, 0);
+        const double percentile = 0.99;
+        const double targetTime = 500; // milliseconds
+
+        var responseTimes = new List<long>(); // milliseconds
+
+        var testTimer = Stopwatch.StartNew();
+        while (testTimer.Elapsed < testDuration)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            var resp = await _httpClient.GetAsync(endpoint);
+
+            Assert.True(resp.IsSuccessStatusCode, $"Status code does not indicate success: {resp.StatusCode}");
+
+            responseTimes.Add(stopwatch.ElapsedMilliseconds);
+            stopwatch.Stop();
+        }
+
+        testTimer.Stop();
+
+        responseTimes.Sort();
+
+        var index = percentile / 100.0 * (responseTimes.Count - 1);
+        var lowerIndex = (int)Math.Floor(index);
+        var upperIndex = (int)Math.Ceiling(index);
+        var weight = index - lowerIndex;
+
+        var x = responseTimes[lowerIndex] * (1 - weight) + responseTimes[upperIndex] * weight;
+
+        _output.WriteLine($"{percentile}-percentile: {x} milliseconds");
+        Assert.True(x < targetTime);
+    }
+}
