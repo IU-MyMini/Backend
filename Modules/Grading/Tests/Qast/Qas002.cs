31adbcae713770825c1371521dc168c8141f7007
Andrei Markov <me@markovav.ru>
2025-11-19 21:34:57 +0300

    Reformat

--- Modules/Grading/Tests/Qast/Qas002.cs
+++ Modules/Grading/Tests/Qast/Qas002.cs
@@ -9,24 +9,24 @@ public class Qas002
     public Qas002()
     {
         const string testBaseUrlEnv = "TEST_BASE_URL";
-        var          baseUrl        = Environment.GetEnvironmentVariable(testBaseUrlEnv);
+        var baseUrl = Environment.GetEnvironmentVariable(testBaseUrlEnv);
         if (baseUrl is null)
             Assert.Fail($"{testBaseUrlEnv} env variable is not defined");
 
-        _httpClient             = new HttpClient();
+        _httpClient = new HttpClient();
         _httpClient.BaseAddress = new Uri(baseUrl);
     }
 
     [Fact]
     public async Task Qast002_2()
     {
-        const string envVar    = "QAST002_2_ENDPOINTS";
-        var          endpoints = Environment.GetEnvironmentVariable(envVar)?.Split(",");
+        const string envVar = "QAST002_2_ENDPOINTS";
+        var endpoints = Environment.GetEnvironmentVariable(envVar)?.Split(",");
 
         if (endpoints is null)
             Assert.Fail($"{envVar} env variable is not defined");
 
-        var tasks   = endpoints.Select(e => _httpClient.GetAsync(e));
+        var tasks = endpoints.Select(e => _httpClient.GetAsync(e));
         var results = await Task.WhenAll(tasks);
 
         foreach (var resp in results)
--- Modules/Grading/Tests/Qast/Qas006.cs
+++ Modules/Grading/Tests/Qast/Qas006.cs
@@ -6,7 +6,7 @@ namespace GradingModule.Tests.Qast;
 
 public class Qas006
 {
-    private readonly HttpClient        _httpClient;
+    private readonly HttpClient _httpClient;
     private readonly ITestOutputHelper _output;
 
     public Qas006(ITestOutputHelper output)
@@ -14,26 +14,26 @@ public class Qas006
         _output = output;
 
         const string testBaseUrlEnv = "TEST_BASE_URL";
-        var          baseUrl        = Environment.GetEnvironmentVariable(testBaseUrlEnv);
+        var baseUrl = Environment.GetEnvironmentVariable(testBaseUrlEnv);
         if (baseUrl is null)
             Assert.Fail($"{testBaseUrlEnv} env variable is not defined");
 
-        _httpClient             = new HttpClient();
+        _httpClient = new HttpClient();
         _httpClient.BaseAddress = new Uri(baseUrl);
     }
 
     [Fact]
     public async Task Qast006_1()
     {
-        const string envVar   = "QAST006_1_ENDPOINT";
-        var          endpoint = Environment.GetEnvironmentVariable(envVar);
+        const string envVar = "QAST006_1_ENDPOINT";
+        var endpoint = Environment.GetEnvironmentVariable(envVar);
 
         if (endpoint is null)
             Assert.Fail($"{envVar} env variable is not defined");
 
-        var          testDuration = new TimeSpan(0, 1, 0);
-        const double percentile   = 0.99;
-        const double targetTime   = 500; // milliseconds
+        var testDuration = new TimeSpan(0, 1, 0);
+        const double percentile = 0.99;
+        const double targetTime = 500; // milliseconds
 
         var responseTimes = new List<long>(); // milliseconds
 
@@ -54,10 +54,10 @@ public class Qas006
 
         responseTimes.Sort();
 
-        var index      = percentile / 100.0 * (responseTimes.Count - 1);
+        var index = percentile / 100.0 * (responseTimes.Count - 1);
         var lowerIndex = (int)Math.Floor(index);
         var upperIndex = (int)Math.Ceiling(index);
-        var weight     = index - lowerIndex;
+        var weight = index - lowerIndex;
 
         var x = responseTimes[lowerIndex] * (1 - weight) + responseTimes[upperIndex] * weight;
 
